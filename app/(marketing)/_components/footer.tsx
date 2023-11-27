123aa2d6dc73e8f62280140e929a57405c244f8f
Berkay <berkaycirak_11@hotmail.com>
2023-11-27 23:51:14 +0300

    feat:convex and clerk is added as an authentication and database

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className="flex items-center w-full p-6 bg-background dark:bg-[#1f1f1f] z-50 ">
       <Logo />
-      <div className="md:ml-auto w-full flex justify-between  md:justify-end    items-center gap-x-2 text-muted-foreground">
+      <div className="md:ml-auto w-full flex justify-between  md:justify-end    items-center gap-x-2 text-muted-foreground ">
         <Button variant="ghost" size="sm">
           Privacy Policy
         </Button>
--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -3,9 +3,14 @@ import useScrollTop from "@/hooks/useScrollTop";
 import React from "react";
 import { cn } from "@/lib/utils";
 import Logo from "./logo";
+import Link from "next/link";
 import { ThemeSwitcher } from "@/components/ui/theme-switcher";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
 
 const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
   return (
     <div
@@ -16,6 +21,37 @@ const Navbar = () => {
     >
       <Logo />
       <div className="flex w-full md:justify-end justify-between items-center gap-x-2">
+        {isLoading && (
+          <>
+            <Button variant="ghost" size="sm" disabled>
+              Sign in
+            </Button>
+            <Button size="sm" disabled>
+              Get Notion Free
+            </Button>
+          </>
+        )}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Sign in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Notion Free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm">
+              <Link href="/documents">Enter Notion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
+
         <ThemeSwitcher />
       </div>
     </div>
